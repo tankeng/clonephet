6737e1c09932778991dbc54718f0453f67b1ae3b
phettest02 <phettest02@gmail.com>
2018-01-17 21:12:20 +0700

    Update Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -20,8 +20,8 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/1.7.14"
-  LA = "CHROMEOS\t1.7.14\tChrome_OS\t1"      
+  UA = "Line/8.0.0"
+  LA = "DESKTOPMAC\t10.10.2-YOSEMITE-x64\tMAC 4.5.0"      
 
   authToken = None
   cert = None
